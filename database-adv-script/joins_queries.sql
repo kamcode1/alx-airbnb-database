5e500f9fd8eba5194f9c56f26102fdf205a9a482
Kam Ephrem <kamnoah88@gmail.com>
2025-07-05 17:41:12 +0300

    added inner join query

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,16 @@
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email
+FROM 
+    Booking b
+INNER JOIN 
+    User u
+ON 
+    b.user_id = u.user_id;
