7f9ad8267afcef81e66e3e5d70f95a551c2fef65
Kam Ephrem <kamnoah88@gmail.com>
2025-07-06 20:15:30 +0300

    Added EXPLAIN & ANALAYZE part in database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -11,3 +11,9 @@ CREATE INDEX idx_property_property_id ON Property(property_id);
 
 -- Indexes for Review table (optional)
 CREATE INDEX idx_review_property_id ON Review(property_id);
+
+EXPLAIN ANALYZE
+SELECT *
+FROM booking b
+INNER JOIN user u ON b.user_id = u.user_id
+WHERE u.email = 'alice@example.com';
